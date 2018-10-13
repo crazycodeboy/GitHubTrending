50cea1be3cc3e5f495b6adbfcfd97c86fa479f86
jph <jph139883@alibaba-inc.com>
2018-10-14 00:00:07 +0800

    publish v3.0.1

--- trending/GitHubTrending.js
+++ trending/GitHubTrending.js
@@ -7,29 +7,29 @@
 import TrendingUtil from './TrendingUtil';
 
 export default class GitHubTrending {
-  GitHubTrending(){//Singleton pattern
-    if (typeof GitHubTrending.instance==='object') {
-      return GitHubTrending.instance;
-    }
-    GitHubTrending.instance=this;
-  }
-  fetchTrending(url){
-    return new Promise((resolve,reject)=>{
-      fetch(url)
-      .then((response)=>response.text())
-      .catch((error)=>{
-        reject(error);
-        console.log(error);
-      }).then((responseData)=>{
-        try {
-          resolve(TrendingUtil.htmlToRepo(responseData));
-        } catch (e) {
-          reject(e);
+    constructor() {//Singleton pattern
+        if (typeof GitHubTrending.instance === 'object') {
+            return GitHubTrending.instance;
         }
-      }).done();
-    });
-  }
+        GitHubTrending.instance = this;
+    }
 
+    fetchTrending(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url)
+                .then((response) => response.text())
+                .catch((error) => {
+                    reject(error);
+                    console.log(error);
+                }).then((responseData) => {
+                try {
+                    resolve(TrendingUtil.htmlToRepo(responseData));
+                } catch (e) {
+                    reject(e);
+                }
+            }).done();
+        });
+    }
 
 
 }
